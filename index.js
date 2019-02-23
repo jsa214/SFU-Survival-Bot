eeded5212446632d47053eb8b9138ee1630afeb9
Kisub Song <ksong@velometro.com>
2019-02-23 13:57:55 -0800

    add road msg

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const Discord = require('discord.js');
- const client = new Discord.Client();
+const {Client, Attachment} = require('discord.js');
+const client = new Client();
+
+//sfu road conditions url
+const sfuRoadConditionsUrl = 'http://www.sfu.ca/security/sfuroadconditions/';
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+    if (msg.content === 'road') {
+        rp(sfuRoadConditionsUrl).then(function(html){
+            //success!
+            const $ = cheerio.load(html);
+            var roadStatus = $('.roadStatus').text();
+
+            // var image1 = $()
+            msg.reply(roadStatus);
+
+        }).catch(function(err){
+            //handle error
+        });;
+    }
+});
 
 client.login('NTQ4OTQ5MzY4OTk3MjgxODEz.D1MwzA.qliWFI_6B8v1uC4v2B5bu7OR7ZM');
