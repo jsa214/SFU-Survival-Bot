48fc1557933bc41bd65600a85e46c0df08639acb
Kisub Song <ksong@velometro.com>
2019-02-24 10:59:42 -0800

    bot cannot listen to itself

--- index.js
+++ index.js
@@ -21,183 +21,197 @@ client.on('ready', () => {
 //bot.guilds.get("548960426440786123").channels.get("548960426440786123").send("Spook!");
 
 client.on('message', msg => {
-    var data;
-    if(carpoolFlag && msg.id != carpoolPromptMsgId) {
-        data = msg.content.split(",");
-        if(data.length != 2 && typeof data[1] == NaN) {
-            msg.channel.send("Wrong arguments");
+
+    if(!msg.author.bot) {
+        msg.reply(msg.channel.type);
+        var data;
+        if(carpoolFlag && msg.id != carpoolPromptMsgId) {
+            data = msg.content.split(",");
+            if(data.length != 2 && typeof data[1] == NaN) {
+                msg.channel.send("Wrong arguments");
+            }
+          //  console.log(data)
+            console.log(msg.content);
+            var channel = client.channels.get('548960426440786123');
+            carpoolFlag = false;
+            channel.send("Going down:\n" + "Time: "+  data[0] + "\nNum of Seats: "+ data[1]+"\n(*React with ✅ on this message to reserve a spot. Private Message will be sent once confirmed)")
+              .then(message => {
+                carpool = new Carpool(message, data[1], data[0], data[2]);
+
+              });
+            //new Carpooldata[0]
+
         }
-      //  console.log(data)
-        console.log(msg.content);
-        var channel = client.channels.get('548960426440786123');
-        carpoolFlag = false;
-        channel.send("Going down:\n" + "Time: "+  data[0] + "\nNum of Seats: "+ data[1]+"\n(*React with ✅ on this message to reserve a spot. Private Message will be sent once confirmed)")
-          .then(message => {
-            carpool = new Carpool(message, data[1], data[0], data[2]);
-
-          });
-        //new Carpooldata[0]
-
-    }
-    if (msg.content.includes('I can drive')){
-        msg.channel.send("What time? How many seat? Where to meet? (ex: 18:30,3,In front of library)")
-          .then(message => {
-            carpoolPromptMsgId = message.id;
-            carpoolFlag = true
-          });
-        //console.log(msg.content);
-    }
-
-
-    if (msg.content.includes('road')) {
-        rp(sfuRoadConditionsUrl).then(function(html){
-            //success!
-            const $ = cheerio.load(html);
-            var roadStatus = $('.roadStatus').text();
-            var webcams = $('.webcams').html();
-            var webcamImgs = [];
-            $('a .webcamimg').each(function(i, elem) {
-                var webcamAlt = $(this).attr('alt');
-                if(webcamAlt === 'Gaglardi intersection' ||
-                    webcamAlt === 'Tower Road South') {
-                    webcamImgs.push($(this).attr('src'));
-                }
-            });
-            msg.channel.send('Roads: ' + roadStatus, {
-                files: webcamImgs
-            });
-        }).catch(function(err){
-            //handle error
-        });;
-    }
-
-    if(msg.content.toLowerCase() === 'bus'){
-        msg.reply(("Route Options: 143, 144, 145, 95"));
-    }
-
-    if(msg.content.toLowerCase() === 'library' || msg.content.toLowerCase()===('lib')){
-        var lib = {
-            uri: 'http://api.lib.sfu.ca/hours/2/summary',
-            json: true
-        };
-        rp(lib).then(function(response){
-          //  var response = $('location').text();
-            //var obj = JSON.parse(response)
-            for (i=0; i<=2; i++){
-              msg.channel.send(response[i].location+"\n"+"\tOpen Time: "
-                                + response[i].open_time+"\n"+ "\tClose Time: "+
-                                response[i].close_time);
-              //msg.channel.send("Open Time: " + response[i].open_time);
-              //msg.reply("Close Time: "+ response[i].close_time);
-            }
-            })
-            .catch(function (err){
-              msg.reply('not working')
-        });
-    }
-
-    if(msg.content=='145'||msg.content=='143'||msg.content=='95'||msg.content=='144'){
-        var link = 'http://api.translink.ca/RTTIAPI/V1/stops/'+bustostop(msg.content)+'/estimates'
-        var bus95 = {
-            uri: link,
-            //'http://api.translink.ca/RTTIAPI/V1/stops/51861/estimates',
-            qs: {
-                apiKey: 'rx3BUkEmtofrx0Dr9JfZ', // -> uri + '?access_token=xxxxx%20xxxxx'
-                count: 1
-            }
-        };
-
-        rp(bus95)
-            .then(function (response) {
-                //console.log(response)
-                //msg.reply(response)
-                //const $ = cheerio.load(html);
-                const $=cheerio.load(response);
-                var cancel = $('CancelledTrip').text();
-                var time = $('ExpectedLeaveTime').text();
-                var busNo = bustostop(msg.content)
-                var image1 = $()
-
-                if (cancel=='false'){
-                cancel = 'Running'
-                }
-                else{
-                cancel ='Cancelled'
-                }
-                msg.reply(msg.content+": ")
-                msg.reply('Status: '+cancel)
-                //msg.reply(msg.content+": "+cancel);
-                msg.reply("Expected Leave Time (Upper Loop): "+time);
-
-            })
-            .catch(function (err) {
-                msg.reply('not working')
-            });
-    }
-
-    if(msg.content.includes('weather')) {
-        var options;
-        if(msg.content.includes('surrey')) {
-            options = {
-                uri: sfuApi,
-                qs: {
-                    key: '2dd8bd4c210b4470127f6bf5255dd993',
-                    lat: 49.187873,
-                    lng: -122.848297,
-                    location: 'surrey'
-                },
-                headers: {
-                    'User-Agent': 'Request-Promise'
-                },
-                json: true // Automatically parses the JSON string in the response
-            };
-        } else if(msg.content.includes('vancouver')) {
-            options = {
-                uri: sfuApi,
-                qs: {
-                    key: '2dd8bd4c210b4470127f6bf5255dd993',
-                    lat: 49.284462,
-                    lng: -123.111719,
-                    location: 'vancouver'
-                },
-                headers: {
-                    'User-Agent': 'Request-Promise'
-                },
-                json: true // Automatically parses the JSON string in the response
-            };
-        } else {
-            options = {
-                uri: sfuApi,
-                qs: {
-                    key: '2dd8bd4c210b4470127f6bf5255dd993',
-                    lat: 49.2792,
-                    lng: -122.9086,
-                    location: 'burnaby'
-                },
-                headers: {
-                    'User-Agent': 'Request-Promise'
-                },
-                json: true // Automatically parses the JSON string in the response
-            };
+        if (msg.content.includes('I can drive')){
+            msg.channel.send("What time? How many seat? Where to meet? (ex: 18:30,3,In front of library)")
+              .then(message => {
+                carpoolPromptMsgId = message.id;
+                carpoolFlag = true
+              });
+            //console.log(msg.content);
         }
 
 
-        rp(options)
-            .then(function (response) {
-                var current = response.currently;
-                var today = response.today;
-                var tomorrow = response.tomorrow;
-                var msgBuilder = 'Current:\n'
-                    + '\t' + current.temperature + ' °\t' + current.summary
-                    + '\nToday:\n'
-                    + '\t' + today.max_temperature + '°/' + today.min_temperature + '°\t' + today.summary
-                    + '\nTomorrow:\n'
-                    + '\t' + tomorrow.max_temperature + '°/' + tomorrow.min_temperature + '°\t' + tomorrow.summary;
-                msg.channel.send(msgBuilder);
-            })
-            .catch(function (err) {
-            });
+        if (msg.content.includes('road')) {
+            rp(sfuRoadConditionsUrl).then(function(html){
+                //success!
+                const $ = cheerio.load(html);
+                var roadStatus = $('.roadStatus').text();
+                var webcams = $('.webcams').html();
+                var webcamImgs = [];
+                $('a .webcamimg').each(function(i, elem) {
+                    var webcamAlt = $(this).attr('alt');
+                    if(webcamAlt === 'Gaglardi intersection' ||
+                        webcamAlt === 'Tower Road South') {
+                        webcamImgs.push($(this).attr('src'));
+                    }
+                });
+                msg.channel.send('Roads: ' + roadStatus, {
+                    files: webcamImgs
+                });
+            }).catch(function(err){
+                //handle error
+            });;
+        }
 
+        if(msg.content.toLowerCase() === 'bus'){
+            msg.reply(("Route Options: 143, 144, 145, 95"));
+        }
+
+        if(msg.content.toLowerCase() === 'library' || msg.content.toLowerCase()===('lib')){
+            var lib = {
+                uri: 'http://api.lib.sfu.ca/hours/2/summary',
+                json: true
+            };
+            rp(lib).then(function(response){
+              //  var response = $('location').text();
+                //var obj = JSON.parse(response)
+                for (i=0; i<=2; i++){
+                  msg.channel.send(response[i].location+"\n"+"\tOpen Time: "
+                                    + response[i].open_time+"\n"+ "\tClose Time: "+
+                                    response[i].close_time);
+                  //msg.channel.send("Open Time: " + response[i].open_time);
+                  //msg.reply("Close Time: "+ response[i].close_time);
+                }
+                })
+                .catch(function (err){
+                  msg.reply('not working')
+            });
+        }
+
+        if(msg.content=='145'||msg.content=='143'||msg.content=='95'||msg.content=='144'){
+            var link = 'http://api.translink.ca/RTTIAPI/V1/stops/'+bustostop(msg.content)+'/estimates'
+            var bus95 = {
+                uri: link,
+                //'http://api.translink.ca/RTTIAPI/V1/stops/51861/estimates',
+                qs: {
+                    apiKey: 'rx3BUkEmtofrx0Dr9JfZ', // -> uri + '?access_token=xxxxx%20xxxxx'
+                    count: 1
+                }
+            };
+
+            rp(bus95)
+                .then(function (response) {
+                    //console.log(response)
+                    //msg.reply(response)
+                    //const $ = cheerio.load(html);
+                    const $=cheerio.load(response);
+                    var cancel = $('CancelledTrip').text();
+                    var time = $('ExpectedLeaveTime').text();
+                    var busNo = bustostop(msg.content)
+                    var image1 = $()
+
+                    if (cancel=='false'){
+                    cancel = 'Running'
+                    }
+                    else{
+                    cancel ='Cancelled'
+                    }
+                    msg.reply(msg.content+": ")
+                    msg.reply('Status: '+cancel)
+                    //msg.reply(msg.content+": "+cancel);
+                    msg.reply("Expected Leave Time (Upper Loop): "+time);
+
+                })
+                .catch(function (err) {
+                    msg.reply('not working')
+                });
+        }
+
+        if(msg.content.includes('weather')) {
+            var options;
+            if(msg.content.includes('surrey')) {
+                options = {
+                    uri: sfuApi,
+                    qs: {
+                        key: '2dd8bd4c210b4470127f6bf5255dd993',
+                        lat: 49.187873,
+                        lng: -122.848297,
+                        location: 'surrey'
+                    },
+                    headers: {
+                        'User-Agent': 'Request-Promise'
+                    },
+                    json: true // Automatically parses the JSON string in the response
+                };
+            } else if(msg.content.includes('vancouver')) {
+                options = {
+                    uri: sfuApi,
+                    qs: {
+                        key: '2dd8bd4c210b4470127f6bf5255dd993',
+                        lat: 49.284462,
+                        lng: -123.111719,
+                        location: 'vancouver'
+                    },
+                    headers: {
+                        'User-Agent': 'Request-Promise'
+                    },
+                    json: true // Automatically parses the JSON string in the response
+                };
+            } else {
+                options = {
+                    uri: sfuApi,
+                    qs: {
+                        key: '2dd8bd4c210b4470127f6bf5255dd993',
+                        lat: 49.2792,
+                        lng: -122.9086,
+                        location: 'burnaby'
+                    },
+                    headers: {
+                        'User-Agent': 'Request-Promise'
+                    },
+                    json: true // Automatically parses the JSON string in the response
+                };
+            }
+
+
+            rp(options)
+                .then(function (response) {
+                    var current = response.currently;
+                    var today = response.today;
+                    var tomorrow = response.tomorrow;
+                    var msgBuilder = 'Current:\n'
+                        + '\t' + current.temperature + ' °\t' + current.summary
+                        + '\nToday:\n'
+                        + '\t' + today.max_temperature + '°/' + today.min_temperature + '°\t' + today.summary
+                        + '\nTomorrow:\n'
+                        + '\t' + tomorrow.max_temperature + '°/' + tomorrow.min_temperature + '°\t' + tomorrow.summary;
+                    msg.channel.send(msgBuilder);
+                })
+                .catch(function (err) {
+                });
+
+        }
+
+        if(msg.content === 'help') {
+            var helpMsg = ''
+                + '\n\t' + 'I can drive'
+                + '\n\t' + 'road'
+                + '\n\t' + 'bus'
+                + '\n\t' + 'library/lib'
+                + '\n\t' + 'weather'
+            msg.channel.send(helpMsg);
+        }
     }
 });
 
